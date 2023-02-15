61f69fb61e9e8787bd234465afea3b5a95ae9a93
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-02-14 22:11:17 -0500

    finished Task C

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // add your code as appropriate
 #include <iostream>
 #include "reservoir.h"
+#include "reverseorder.h"
 int main()
 {
 
@@ -8,4 +9,12 @@ int main()
   std::cout << get_east_storage("03/02/2018") << " billion gallons" << std::endl;
   std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
   std::cout << "maximum storage in East basin: " << get_max_east() << " billion gallons" << std::endl;
+  std::cout << compare_basins("01/01/2018") << std::endl;
+  std::cout << compare_basins("02/01/2018") << std::endl;
+  std::cout << compare_basins("03/01/2018") << std::endl;
+  std::cout << compare_basins("04/01/2018") << std::endl;
+  std::cout << "The data in reverse order from the interval is displayed below:" << std::endl;
+  reverse_order("01/01/2018", "01/05/2018");
+  std::cout << "The data in reverse order from the interval is displayed below:" << std::endl;
+  reverse_order("01/01/2018", "01/15/2018");
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -99,3 +99,49 @@ double get_max_east()
     data_file.close(); // closing the stream
     return maxStorage; // if the date is not found in the file return -1
 }
+
+std::string compare_basins(std::string date)
+{
+    std::ifstream data_file("Current_Reservoir_Levels.tsv"); // required for file input
+
+    if (data_file.fail()) // if program can't open file
+    {
+        std::cerr << "File cannot be opened for reading." << std::endl; // error message
+        exit(1);                                                        // exit if failed to open the file
+    }
+
+    // skipping first line because the first line is not data but headers
+    std::string junk;
+    std::getline(data_file, junk);
+
+    std::string dataDate;
+    double eastStorage;
+    double eastElevation;
+    double westStorage; // data from csv
+    double westElevation;
+
+    while (data_file >> dataDate >> eastStorage >> eastElevation >> westStorage >> westElevation) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
+    {
+
+        if (date == dataDate) // if the date is found
+        {
+            if (eastElevation > westElevation)
+            {
+                return "East";
+            }
+            else if (eastElevation < westElevation)
+            {
+                return "West";
+            }
+            else
+            {
+                return "Equal";
+            }
+        }
+
+        data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
+    }
+
+    data_file.close(); // closing the stream
+    return 0;          // if the date is not found in the file return -1
+}
