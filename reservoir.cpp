76383f027fc9596d58368eb7cbf410656b0b402b
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-02-14 23:59:38 -0500

    Updated Makefile along with comments to code

--- reservoir.cpp
+++ reservoir.cpp
@@ -12,8 +12,7 @@ double get_east_storage(std::string date)
         exit(1);                                                        // exit if failed to open the file
     }
 
-    // skipping first line because the first line is not data but headers
-    std::string junk;
+    std::string junk; // skipping first line because the first line is not data but headers
     std::getline(data_file, junk);
 
     std::string dataDate;
@@ -30,8 +29,8 @@ double get_east_storage(std::string date)
         data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
     }
 
-    data_file.close(); // closing the stream
-    return 0;          // if the date is not found in the file return -1
+    data_file.close();
+    return 0;
 }
 
 double get_min_east()
@@ -55,16 +54,16 @@ double get_min_east()
     while (data_file >> dataDate >> eastStorage) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
     {
 
-        if (eastStorage < minStorage)
+        if (eastStorage < minStorage) // if the value of east basin storage is less than the current minimum value
         {
-            minStorage = eastStorage;
+            minStorage = eastStorage; // set the minimum value to the current value of east basin storage
         }
 
         data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
     }
 
     data_file.close(); // closing the stream
-    return minStorage; // if the date is not found in the file return -1
+    return minStorage;
 }
 
 double get_max_east()
@@ -88,16 +87,16 @@ double get_max_east()
     while (data_file >> dataDate >> eastStorage) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
     {
 
-        if (eastStorage > maxStorage)
+        if (eastStorage > maxStorage) // if the value of east basin storage is greater than the current maximum value
         {
-            maxStorage = eastStorage;
+            maxStorage = eastStorage; // set the maximum value to the current value of east basin storage
         }
 
         data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
     }
 
     data_file.close(); // closing the stream
-    return maxStorage; // if the date is not found in the file return -1
+    return maxStorage;
 }
 
 std::string compare_basins(std::string date)
@@ -125,17 +124,17 @@ std::string compare_basins(std::string date)
 
         if (date == dataDate) // if the date is found
         {
-            if (eastElevation > westElevation)
+            if (eastElevation > westElevation) // if the elevation of east basin is greater than the elevation of west basin
             {
-                return "East";
+                return "East"; // return "East"
             }
-            else if (eastElevation < westElevation)
+            else if (eastElevation < westElevation) // if the elevation of east basin is less than the elevation of west basin
             {
-                return "West";
+                return "West"; // return "West"
             }
             else
             {
-                return "Equal";
+                return "Equal"; // return "Equal"
             }
         }
 
@@ -143,5 +142,5 @@ std::string compare_basins(std::string date)
     }
 
     data_file.close(); // closing the stream
-    return 0;          // if the date is not found in the file return -1
+    return 0;
 }
--- reverseorder.cpp
+++ reverseorder.cpp
@@ -6,15 +6,14 @@ void reverse_order(std::string
                    std::string date2)
 {
 
-    std::ifstream data_file("Current_Reservoir_Levels.tsv"); // required for file input
+    std::ifstream data_file("Current_Reservoir_Levels.tsv"); // opens the file for reading
     if (data_file.fail())                                    // if program can't open file
     {
         std::cerr << "File cannot be opened for reading." << std::endl; // error message
         exit(1);                                                        // exit if failed to open the file
     }
 
-    // skipping first line because the first line is not data but headers
-    std::string junk;
+    std::string junk; // skipping first line because the first line is not data but headers
     std::getline(data_file, junk);
 
     std::string dataDate;
@@ -25,32 +24,32 @@ void reverse_order(std::string
     double westArray[365];
     std::string dateArray[365];
     int index = 0;
-    bool startWrite = false;
+    bool startWrite = false; // indicates whether to start writing to the array or not based on a condition which is if the date is equal to the date1.
 
-    while (data_file >> dataDate >> eastStorage >> eastElevation >> westStorage >> westElevation) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
+    while (data_file >> dataDate >> eastStorage >> eastElevation >> westStorage >> westElevation) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file. Variable names should match the names in the file in order.
     {
-        if (dataDate == date1)
+        if (dataDate == date1) // if the date in the file is equal to the date1 during the loop
         {
-            startWrite = true;
+            startWrite = true; // set the startWrite to true so that the data can be written to the array
         }
 
-        if (startWrite)
+        if (startWrite) // if startWrite is true
         {
-            westArray[index] = westStorage;
-            dateArray[index] = dataDate;
-            index++;
+            westArray[index] = westStorage; // write the storage data to the westArray
+            dateArray[index] = dataDate;    // write the current date to the dateArray
+            index++;                        // increment the index
         }
-        if (dataDate == date2)
+        if (dataDate == date2) // if the date in the file is equal to the date2 during the loop
         {
-            startWrite = false;
+            startWrite = false; // set the startWrite to false so that the data cannot be written to the array
         }
 
         data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
     }
 
-    data_file.close(); // closing the stream
-    for (int i = index - 1; i >= 0; i--)
+    data_file.close();                   // closing the stream
+    for (int i = index - 1; i >= 0; i--) // print the array in reverse order. The index is decremented by 1 because the index is incremented by 1 at the end of the loop. So the index will be 1 more than the actual number of elements in the array.
     {
-        std::cout << dateArray[i] << " " << westArray[i] << std::endl;
+        std::cout << dateArray[i] << " " << westArray[i] << std::endl; // print the date and the storage data at the index i
     }
 }
