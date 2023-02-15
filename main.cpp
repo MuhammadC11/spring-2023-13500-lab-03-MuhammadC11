5d0a08e36ee6c79e98b52c4bbe8a335b0c69da4c
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-02-14 20:43:23 -0500

    finished Lab 3 Task A and B

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-// add your code as appropriate 
+// add your code as appropriate
 #include <iostream>
-
+#include "reservoir.h"
 int main()
 {
-  
 
-  
-  return 0;
+  std::cout << get_east_storage("01/01/2018") << " billion gallons" << std::endl;
+  std::cout << get_east_storage("03/02/2018") << " billion gallons" << std::endl;
+  std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
+  std::cout << "maximum storage in East basin: " << get_max_east() << " billion gallons" << std::endl;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -4,9 +4,9 @@
 double get_east_storage(std::string date)
 {
 
-    std::ifstream inp_file("Current_Reservoir_Levels.tsv"); // required for file input
+    std::ifstream data_file("Current_Reservoir_Levels.tsv"); // required for file input
 
-    if (inp_file.fail()) // if program can't open file
+    if (data_file.fail()) // if program can't open file
     {
         std::cerr << "File cannot be opened for reading." << std::endl; // error message
         exit(1);                                                        // exit if failed to open the file
@@ -14,23 +14,88 @@ double get_east_storage(std::string date)
 
     // skipping first line because the first line is not data but headers
     std::string junk;
-    std::getline(inp_file, junk);
+    std::getline(data_file, junk);
 
-    // file reading
+    std::string dataDate;
+    double eastStorage; // data from csv
 
-    std::string entry_date, east_basin_storage; // data from csv
-
-    while (inp_file >> entry_date >> east_basin_storage)
+    while (data_file >> dataDate >> eastStorage) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
     {
 
-        if (date == entry_date)
+        if (date == dataDate) // if the date is found
         {
-            return std::stold(east_basin_storage);
+            return eastStorage; // return the value of east basin storage as a double value and exit the function
         }
 
-        inp_file.ignore(INT_MAX, '\n'); // skips to the end of line,
+        data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
     }
 
-    inp_file.close(); // closing the stream
-    return -1;
+    data_file.close(); // closing the stream
+    return 0;          // if the date is not found in the file return -1
+}
+
+double get_min_east()
+{
+    std::ifstream data_file("Current_Reservoir_Levels.tsv"); // required for file input
+
+    if (data_file.fail()) // if program can't open file
+    {
+        std::cerr << "File cannot be opened for reading." << std::endl; // error message
+        exit(1);                                                        // exit if failed to open the file
+    }
+
+    // skipping first line because the first line is not data but headers
+    std::string junk;
+    std::getline(data_file, junk);
+
+    std::string dataDate;
+    double eastStorage;              // data from csv
+    double minStorage = 10000000000; // variable to store the minimum value of east basin storage starting at first value in the file
+
+    while (data_file >> dataDate >> eastStorage) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
+    {
+
+        if (eastStorage < minStorage)
+        {
+            minStorage = eastStorage;
+        }
+
+        data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
+    }
+
+    data_file.close(); // closing the stream
+    return minStorage; // if the date is not found in the file return -1
+}
+
+double get_max_east()
+{
+    std::ifstream data_file("Current_Reservoir_Levels.tsv"); // required for file input
+
+    if (data_file.fail()) // if program can't open file
+    {
+        std::cerr << "File cannot be opened for reading." << std::endl; // error message
+        exit(1);                                                        // exit if failed to open the file
+    }
+
+    // skipping first line because the first line is not data but headers
+    std::string junk;
+    std::getline(data_file, junk);
+
+    std::string dataDate;
+    double eastStorage;    // data from csv
+    double maxStorage = 0; // variable to store the minimum value of east basin storage starting at first value in the file
+
+    while (data_file >> dataDate >> eastStorage) // reads the file line by line and stores it in the variables declared above until the file ends or an error occurs in reading the file.
+    {
+
+        if (eastStorage > maxStorage)
+        {
+            maxStorage = eastStorage;
+        }
+
+        data_file.ignore(INT_MAX, '\n'); // skips to the end of line, ignores the remaining columns in the line and moves to the next line in the file to read the next line of data in the file. INT_MAX is the maximum value of int. As soon as it reads east storage, it will ignore the rest of the line and move to the next line.
+    }
+
+    data_file.close(); // closing the stream
+    return maxStorage; // if the date is not found in the file return -1
 }
